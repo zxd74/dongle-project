f191f8f19903a7fa4261ee7489253ec61a840f15
Dongle <zhengxd@cread.com>
2023-04-22 22:54:01 +0800

    update question system doc

--- sys-question/question-server/src/main/java/com/dongle/question/utils/QuestionType.java
+++ sys-question/question-server/src/main/java/com/dongle/question/utils/QuestionType.java
@@ -2,16 +2,16 @@ package com.dongle.question.utils;
 
 public enum QuestionType {
     /**
-     * 0 SINGLE 单选题
-     * 1 MULTI 多选题
-     * 2 FILL 填空题
-     * 3 RESPONSE 解答题
-     * 4 TITLE 作文题
-     * 5 DRAW 画图题
-     * 6 MATCH 连线题
-     * 7 TRUE_OR_FALSE 判断题
+     * 1 SINGLE 单选题
+     * 2 MULTI 多选题
+     * 3 FILL 填空题
+     * 4 RESPONSE 解答题
+     * 5 TITLE 作文题
+     * 6 DRAW 画图题
+     * 7 MATCH 连线题
+     * 8 TRUE_OR_FALSE 判断题
      */
-    SINGLE(0),MULTI(1),FILL(2),RESPONSE(3),TITLE(4),DRAW(5),MATCH(6),TRUE_OR_FALSE(7);
+    SINGLE(1),MULTI(2),FILL(3),RESPONSE(4),TITLE(5),DRAW(6),MATCH(7),TRUE_OR_FALSE(8);
 
     private final int value;
 
