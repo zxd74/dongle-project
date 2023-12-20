59e80a050f8db7e03a3e1afdf39b179558c5262b
Dongle <dongle74@163.com>
2023-12-20 19:11:58 +0800

    更新股票服务

--- sys-stock/dongle-stock-service/src/main/java/com/dongle/stock/dao/StockHistoryDataDao.java
+++ sys-stock/dongle-stock-service/src/main/java/com/dongle/stock/dao/StockHistoryDataDao.java
@@ -8,8 +8,9 @@ import java.util.List;
 
 public interface StockHistoryDataDao extends CrudRepository<StockHistoryData,StockHistoryKey> {
 
-    @Query(value="select * from stock_history_data where code=?1 order by date limit ?2",nativeQuery = true)
+    @Query(value="select * from stock_day_data where code=?1 order by date limit ?2",nativeQuery = true)
     List<StockHistoryData> queryByCode(String code,int day);
+    
 
     @Query(value = "select sh from StockHistoryData sh where sh.date = (SELECT MAX(shd.date) from StockHistoryData shd) order by sh.code")
     List<StockHistoryData> queryByNew();
