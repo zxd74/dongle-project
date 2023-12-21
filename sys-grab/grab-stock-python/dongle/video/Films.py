0691ec403feb25056ebb65c64173ad837ef8463e
Dongle <dongle74@163.com>
2023-12-22 07:04:28 +0800

    完善视频抓取脚本

--- sys-grab/grab-stock-python/dongle/video/Films.py
+++ sys-grab/grab-stock-python/dongle/video/Films.py
@@ -11,6 +11,13 @@ import re,json
 def requestHtml(url):
     return requests.get(url).content.decode('utf-8')
 
+def resovleUrlRootPath(url):
+    '''
+        获取URL 根路径
+    '''
+    urls = urllib.parse.urlparse(url)
+    return urls.scheme + "://" + urls.netloc
+
 def resolveVideoName(soup):
     title = soup.title.string
     pattern=re.compile(r"《.*》")
@@ -47,54 +54,63 @@ def getRealM3u8(url):
     else: # 真实m3u8链接
         return url
 
+import urllib,base64
+def urlDecode(url,type=0):
+    """
+        url内容解密
+    """
+    if type==0: return url
+    if type==1: return urllib.parse.unquote(url)
+    if type==2: return urllib.parse.unquote(base64.b64decode(url).decode("utf-8"))
+    raise Exception("unknown type")
+
+
 def formatFfmpeg(videos):
+    print("----------------format m3u8 to ffmpeg---------------")
     for video in videos:
         print("nohup ffmpeg -i %s -vcodec copy resources/%s.mp4 > ff.log 2>&1 &" % (video['m3u8'],video['name']))
 
-def videoFromXigua543(index):
-    """
-        https://www.xigua543.com/
-    """
-    host = "https://www.xigua543.com/"
-    content = requestHtml(index)
-    soup = BeautifulSoup(content, 'html.parser')
-    videos  = resolveVideoList(soup,el=None,id='con_playlist_1')
-    name = resolveVideoName(soup)
+def formatUrlShow(videos):
+    print("----------------show m3u8 url:")
     for video in videos:
+        print("[%s] %s" % (video['name'],video['m3u8']))
+
+def resolveVideo(home,id,cls,pattern,decodeType=0):
+    """
+    @home 视频介绍页
+    @id 视频集目录id
+    @cls 视频集目录class，与id二者取其一，id优先
+    @pattern 视频播放内容解析脚本变量，从script
+    @decode_type 视频路径解密方式，0-无需解密，1-url解密，2-url+base64解密
+    """
+    host = resovleUrlRootPath(home) 
+    content = requestHtml(home)
+    soup = BeautifulSoup(content, 'html.parser')
+    videos  = resolveVideoList(soup,el=None,id=id,cls=cls)
+    name = resolveVideoName(soup)
+    print("film name:",name)
+    for video in videos:
+        # print(video['url'])
         content = requestHtml(host + video['url'])
         soup = BeautifulSoup(content, 'html.parser')
-        url = resolveVideoContentFromScript(soup,r"var zanpiancms_player =(.*?)$")
+        url = resolveVideoContentFromScript(soup,pattern)
+        url = urlDecode(url,decodeType)
         video['m3u8'] = getRealM3u8(url)
         video['name'] = name + video['name']
-    formatFfmpeg(videos)
-
-
-def videoFromHdmoli(index):
-    """
-        https://www.hdmoli.com/
-    """
-    def hdmoliDecode(content):
-        import urllib,base64
-        return urllib.parse.unquote(base64.b64decode(content).decode("utf-8"))
     
-    host = "https://www.hdmoli.top/"
-    content = requestHtml(index)
-    soup = BeautifulSoup(content, 'html.parser')
-    videos  = resolveVideoList(soup,el=None,id='panel1')
-    name = resolveVideoName(soup)
-    for video in videos:
-        content = requestHtml(host + video['url'])
-        soup = BeautifulSoup(content, 'html.parser')
-        url = resolveVideoContentFromScript(soup,r"var player_aaaa=(.*?)$",defun=hdmoliDecode)
-        video['m3u8'] = getRealM3u8(url)
-        video['name'] = name + video['name']
-    formatFfmpeg(videos)
+    formatUrlShow(videos)
 
 def videoFrom(url):
-    if url.find("www.xigua543.com") != -1:
-        videoFromXigua543(url)
-    elif url.find("www.hdmoli.top") != -1:
-        videoFromHdmoli(url)
+    rootUrl = resovleUrlRootPath(url);
+    print("rootUrl:",rootUrl)
+    if rootUrl.find("www.xigua543.com") != -1:
+        # videoFromXigua543(url)
+        resolveVideo(url,'con_playlist_1',None,r"var zanpiancms_player =(.*?)$")
+    elif rootUrl.find("www.hdmoli.top") != -1:
+        # videoFromHdmoli(url)
+        resolveVideo(url,'panel1',None,r"var player_aaaa=(.*?)$",decodeType=2)
+    elif rootUrl.find("libvio.one") != -1:
+        resolveVideo(url,None,"stui-content__playlist",r"var player_aaaa=(.*?)$",decodeType=2)
 
 if __name__ == '__main__':
-    videoFrom("https://www.xigua543.com/dongzuopian/jingqiduichang2/")
+    videoFrom("https://libvio.one/detail/28519.html")
--- sys-grab/grab-stock-python/dongle/video/m3u8.py
+++ sys-grab/grab-stock-python/dongle/video/m3u8.py
@@ -1,18 +1,41 @@
 import requests
 import queue,datetime,threading,os
+import uuid
 
-url = ""
-DIR = r""
-source_file = DIR + "list.txt"
-target_file = DIR + "video.mp4"
-if os.path.exists(DIR) is False:
-    print("Please create a download directory first!")
-    exit(-1)
-isAes = False
+import sys
+
+url = sys.argv[1] or ""
+file_name = sys.argv[2] or "video"
+file_num = sys.argv[3] or "01"
+
+if url == "":
+    raise Exception("Please input url!")
+
+print("receive param: %s %s %s" % (url,file_name,file_num))
+
+DIR = "/data/temp/video/" # 根目录
+TARGET_DIR = DIR + file_name + "/" # 目标目录，mp4存放目录
+if os.path.exists(TARGET_DIR) is False:
+    os.makedirs(TARGET_DIR)
+target_file = "%s%s-%s.mp4" % (TARGET_DIR,file_name,file_num)
+
+import hashlib
+identy = hashlib.md5(url.encode('utf-8')).hexdigest()
+TMP_DIR = DIR  + identy +"/" # 临时目录
+if os.path.exists(TMP_DIR) is False:
+    os.makedirs(TMP_DIR)
+source_file = TMP_DIR + identy + "-tmp.txt"
+ts_file = TMP_DIR + identy + "-ts.txt"
+
 def handlerPrefix(url): # 解析前缀
     return "".join(url[:url.rfind("/")+1])
 
 def resolveM3u8(url,suffix=None,prefix=None): # 解析m3u8 获取ts列表
+    if os.path.exists(ts_file):
+        with open(ts_file,'r',encoding='utf-8') as f:
+            content = f.read()
+            return content.split("\n")
+    
     if prefix is None:
         prefix = handlerPrefix(url)
     
@@ -21,8 +44,7 @@ def resolveM3u8(url,suffix=None,prefix=None): # 解析m3u8 获取ts列表
     for line in content.split("\n"):
         if line.startswith("#EXT-X-KEY"):
             # 加密处理
-            isAes = True
-            print("视频已加密，暂时无法处理！")
+            print("视频已被加密，暂时无法处理！")
             return None
         if line.startswith("#") or line == '':
             continue
@@ -35,11 +57,13 @@ def resolveM3u8(url,suffix=None,prefix=None): # 解析m3u8 获取ts列表
     return list
 
 def generatorListFile(list,filename): # 格式化内容
+    if os.path.exists(file_name):
+        os.remove(file_name)
     with open(filename, "w") as f:
         for i in range(len(list)):
             file = "%d.ts" % i
             list[i]['filename'] = file
-            f.write("file " + DIR + file + "\n")
+            f.write("file " + TMP_DIR + file + "\n")
 
 def convertQueue(list): # 绑定queue
     qe = queue.Queue()
@@ -62,8 +86,10 @@ def downByThread(func,qe): # 多线程下载
     for t in threads:
         t.join()
 
-
 def downTs(url,filename): # 下载ts
+    if os.path.exists(filename):
+        print("file exists：%s" % filename)
+        return
     try:
         r = requests.get(url,stream=True)
         with open(filename, 'wb') as f:
@@ -81,7 +107,7 @@ def down(qe): # 执行下载逻辑
         url= ts['url']
         filename = ts['filename']
         try:
-            downTs(url,DIR + filename)
+            downTs(url,TMP_DIR + filename)
             print("Thread[%s] download %s is success[%s],use time[%s]!" % (thn,url,filename,datetime.datetime.now() - start))
         except Exception as e:
             print("[ERROR][DOWNLOAD]%s download is error!" % filename)
@@ -93,26 +119,28 @@ def merge(list_file,filename): # 合并ts为MP4
     command = "ffmpeg -y -f concat -safe 0 -i %s -vcodec copy %s" % (list_file,filename)
     print("exec video merge system command:",command)
     try:
-        os.system(command)
-        print("video merge is complate!")
+        # os.system(command)
+        import subprocess
+        p = subprocess.Popen(command, shell=True)
+        print("video merge is complate!",p.wait())
     except Exception as ex:
         print("video merge is error!")
         raise ex
 
 def delTsFile(): # 删除无用文件
-    for root, dirs, files in os.walk(DIR):
-        for file in files:
-            if file.endswith('.ts'):
-                os.remove(os.path.join(root, file))
+    # os.delete(TMP_DIR)
+    import shutil
+    shutil.rmtree(TMP_DIR)
+    # os.remove(TMP_DIR) # 无权删除，拒绝访问
 
 def repair(list): # 修复使用，下载缺失片段
     for item in list:
         url = item['url']
         filename = item['filename']
-        if os.path.exists(DIR + filename):
+        if os.path.exists(TMP_DIR + filename):
             continue
         try:
-            downTs(url,DIR + filename)
+            downTs(url,TMP_DIR + filename)
             print("download %s is success[%s]" % (url,filename))
         except Exception as e:
             print("[ERROR][DOWNLOAD]%s download is error!" % filename)
@@ -124,9 +152,9 @@ if __name__ == "__main__":
     if list:
         print("ready down ts count[%d]" % len(list))
         generatorListFile(list,source_file)
-        # repair(list)
         qe = convertQueue(list)
         downByThread(down,qe)
+        print("--- download complete,ready merge ts file ---")
         merge(source_file,target_file)
         delTsFile()
     print("use time[%s]!" % str(datetime.datetime.now() - start))
